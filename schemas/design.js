0534ae4d60dd65b8a71563d37f09f29468ef76dc
josbyte-dev <ruizm.aj@gmail.com>
2023-01-04 21:46:29 -0600

    added gallery to design schema

--- schemas/design.js
+++ schemas/design.js
@@ -25,6 +25,31 @@ export default {
             },
         },
         {
+            name: 'images',
+            type: 'array',
+            title: 'Images',
+            of: [
+                {
+                    name: 'image',
+                    type: 'image',
+                    title: 'Image',
+                    options: {
+                        hotspot: true,
+                    },
+                    fields: [
+                        {
+                            name: 'alt',
+                            type: 'string',
+                            title: 'Alternative text',
+                        },
+                    ],
+                },
+            ],
+            options: {
+                layout: 'grid',
+            },
+        },
+        {
             name: 'description',
             type: 'text',
         },
